d34b0417fb4afe9c19a5a4c2f013632d6525fc67
FernandoCR1976 <fernandocorralesm76@gmail.com>
2021-03-25 02:48:28 +0000

    proyecto terminado

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, Date 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,26 +8,66 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Login(Base):
+    __tablename__ = 'Login'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    Nombre_Usuario = Column(String(20))
+    Primer_Apellido = Column(String(20))
+    Password = Column(String(20))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'User'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    Nickname = Column(String(20))
+    Correo = Column(String(20))
+    Telefono = Column(String(8))
+    Nombre_Usuario = Column(Integer, ForeignKey('Login.Nombre_Usuario'))
+    Login = relationship(Login)
+
+class Like(Base):
+    __tablename__ = 'Like'
+    id = Column(Integer, primary_key=True)
+    CantidadLike = Column(Integer)
+    NombreUsuario = Column(String(25))
+    Nickname = Column(String(15))
+
+class Publicacion(Base):
+    __tablename__ = 'Publicacion'
+    id = Column(Integer, primary_key=True)
+    Titulo  = Column(String(15))
+    Foto = Column(String(25))
+    Texto = Column(String(50))
+    CantidadLike = Column(Integer, ForeignKey('Like.CantidadLike'))
+    Like = relationship(Like)
+
+class Home(Base):
+    __tablename__ = 'Home'
+    id = Column(Integer, primary_key=True)
+    N_Publicaciones = Column(Integer)
+    N_Seguidos = Column(Integer)
+    Biografia = Column(String(150))
+    Publicacion_id = Column(Integer, ForeignKey('Publicacion.id'))
+    Publicacion = relationship(Publicacion)
+    Usuario_id = Column(Integer, ForeignKey('User.id'))
+    Nickname = Column(Integer, ForeignKey('User.Nickname'))
+    Nombre_Usuario = Column(Integer, ForeignKey('User.Nombre_Usuario'))
+    User = relationship(User)
+
+    
+    
+
+
+
+
 
     def to_dict(self):
         return {}
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
+
+
+
+## ForeignKey: Relación uno a muchos... (un carro tiene un fabricante, pero un fabricante puede tener muchos carros)
+## Integer: Esto devuelve numeros enteros.
+## Booleano: Una forma de representar datos binarios, es decir dos valores unicamente 0 y 1. (True es 1 y 0 es False).
